33537aba744c6a10e9f111f6c0e971f8ac743ab0
JacoBM04 <Jacob.barrera@janusautomation.com>
2023-01-05 11:07:24 -0600

    addControllers

---
+++ TeachNotify/Controllers/AnunciosController.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeachNotify.Controllers
+{
+    public class AnunciosController : Controller
+    {
+
+        #region Views
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Helper Methods
+        public static string GetUrlPart(string[] url, int position)
+        {
+            return url.Length >= position + 1 ? url[position].ToString() : "";
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ TeachNotify/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeachNotify.Controllers
+{
+    public class AnunciosController : Controller
+    {
+    
+    }
+    
+}
