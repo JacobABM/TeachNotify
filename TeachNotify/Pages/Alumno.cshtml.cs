5d9cf3b017d7473951b1cf0ecc454b3109e376a4
JacoBM04 <Jacob.barrera@janusautomation.com>
2023-01-05 00:42:41 -0600

    Agregar archivos de proyecto.

---
+++ TeachNotify/Pages/Alumno.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeachNotify.Pages
+{
+    public class AlumnoModel : PageModel
+    {
+        private readonly ILogger<AlumnoModel> _logger;
+
+        public AlumnoModel(ILogger<AlumnoModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TeachNotify/Pages/Docente.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeachNotify.Pages
+{
+    public class DocenteModel : PageModel
+    {
+        private readonly ILogger<DocenteModel> _logger;
+
+        public DocenteModel(ILogger<DocenteModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
